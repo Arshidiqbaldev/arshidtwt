16be2132b7e3c54a3304c5729db13a275900b88d
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-06-20 17:35:45 +0500

    menu added-03

--- myscript.js
+++
@@ -1,9 +0,0 @@
-const mobMenu = document.getElementById("mob");
-
-const menuIcon = document.getElementById("menu");
-
-
-menuIcon.addEventListener("click", () => {
-    mobMenu.classList.toggle("max-h-0");
-    mobMenu.classList.toggle("max-h-50")
-})
