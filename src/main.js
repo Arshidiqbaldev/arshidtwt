1698568fbb4d331caaebaca71817c3f00085e906
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-06-20 17:41:12 +0500

    c js added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
 import './style.css'
 
 
-const mobMenu = document.getElementById("mob");
-
-const menuIcon = document.getElementById("menu");
-
-
-menuIcon.addEventListener("click", () => {
-    mobMenu.classList.toggle("max-h-0");
-    mobMenu.classList.toggle("max-h-50");
-})
