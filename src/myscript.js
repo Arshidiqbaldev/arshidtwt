2bedc645e70bf5787d603596ef37f7283d05da06
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-06-23 12:27:34 +0500

    toggle

--- src/myscript.js
+++ src/myscript.js
@@ -3,7 +3,29 @@ const mobMenu = document.getElementById("mob");
 const menuIcon = document.getElementById("menu");
 
 
-menuIcon.addEventListener("click", () => {
+
+
+
+
+
+
+
+
+// toogle 
+
+const toggleMenu = document.querySelector(".toggle-icon");
+const tog1 = document.querySelectorAll(".toggle-bar");
+
+
+toggleMenu.addEventListener("click", () => {
+
+
+
+    tog1.forEach((i => {
+        i.classList.toggle("toggle-bar-active");
+        i.classList.toggle("bg-black");
+    }));
+
     mobMenu.classList.toggle("max-h-0");
     mobMenu.classList.toggle("max-h-50");
 })
