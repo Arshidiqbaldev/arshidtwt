ea5c844ecac694650d97f9db7404bf5b9b7c65b6
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-06-27 16:43:21 +0500

    f-v

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             input: {
                 main: 'index.html',
                 menu: 'menupage.html',
-                future: "future.html"
+                future: "future.html",
 
             },
         },
